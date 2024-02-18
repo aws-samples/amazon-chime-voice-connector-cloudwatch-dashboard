807307a0eb44a26a4097a78c400a74dfd9979289
Court Schuett <schuettc@amazon.com>
2024-02-17 18:44:05 -0600

    fix: update schedule

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,17 @@
 const { awscdk } = require('projen');
+const { JobPermission } = require('projen/lib/github/workflows-model');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.86.0',
+  cdkVersion: '2.126.0',
   license: 'MIT-0',
   author: 'Court Schuett',
   copyrightOwner: 'Amazon.com, Inc.',
   authorAddress: 'https://aws.amazon.com',
   defaultReleaseBranch: 'main',
-  workflowNodeVersion: '16.x',
+  workflowNodeVersion: '18.x',
+  projenrcTs: true,
+  jest: false,
   appEntrypoint: 'amazon-chime-voiceconnector-cloudwatch-dashboard.ts',
   name: 'amazon-chime-voiceconnector-cloudwatch-dashboard',
   deps: [
@@ -22,6 +27,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     ignoreProjen: false,
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
     },
   },
   autoApproveOptions: {
@@ -30,16 +36,38 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   },
   autoApproveUpgrades: true,
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
-  defaultReleaseBranch: 'main',
 });
 
 const common_exclude = [
+  'docker-compose.yaml',
   'cdk.out',
-  'cdk.context.json',
   'yarn-error.log',
   'dependabot.yml',
   '.DS_Store',
+  '**/dist/**',
+  '.env',
 ];
+
+project.tsconfigDev.file.addOverride('include', [
+  'src/**/*.ts',
+  './.projenrc.ts',
+]);
+
+project.eslint.addOverride({
+  files: ['src/resources/**/*.ts'],
+  rules: {
+    'indent': 'off',
+    '@typescript-eslint/indent': 'off',
+  },
+});
+
+project.eslint.addOverride({
+  files: ['src/resources/**/*.ts'],
+  rules: {
+    '@typescript-eslint/no-require-imports': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+});
 
 project.addTask('launch', {
   exec: 'yarn && yarn projen && yarn build && yarn cdk bootstrap && yarn cdk deploy --all --require-approval never',
